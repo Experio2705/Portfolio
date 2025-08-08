fbfee640c728dbb966b10605c4691a3ae6efcb67
Experio2705 <aryanshinde2705@gmail.com>
2025-08-09 00:46:02 +0530

    Changes in Home

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -6,8 +6,8 @@ function Type() {
     <Typewriter
       options={{
         strings: [
-          "Software Developer",
           "Student",
+          "Software Developer",
           "Data Analyst",
           "Enthusiastic Coder",
         ],
